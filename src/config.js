452afd67422f1f2cf42cd24a127626d535ff6930
M Rohit <mrohit582003@gmail.com>
2024-07-13 22:26:18 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  google: {
+    API_KEY: "",
+    CLIENT_ID: "",
+    SECRET: "",
+  },
+  facebook: {
+    APP_ID: "",
+  },
+  api: {
+    API_URL: "http://192.168.0.99:5000",
+  }
+};
+module.exports = {
+  google: {
+    API_KEY: "",
+    CLIENT_ID: "",
+    SECRET: "",
+  },
+  facebook: {
+    APP_ID: "",
+  },
+  api: {
+    API_URL: "http://45.124.144.253:9890",
+  }
+};
